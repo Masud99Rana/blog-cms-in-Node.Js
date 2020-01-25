b1b353f11384dbf569e9a07bcfddc9b072e10d4d
Masud99Rana <Masudrana917775@gmail.com>
2020-01-25 22:14:24 +0600

    db connected

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+
+
+// DB connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/mr-cms',{ useNewUrlParser: true, useUnifiedTopology: true })
+	.then(db =>{
+    	console.log('mongoDB connected');
+	}).catch(error=> console.log(error));
 
 
 // Using Static
@@ -15,10 +25,12 @@ app.set('view engine', 'handlebars');
 // Load Routes
 const home = require('./routes/home/index');
 const admin = require('./routes/admin/index');
+const posts = require('./routes/admin/posts');
 
 // Use Routes
 app.use('/', home);
 app.use('/admin', admin);
+app.use('/admin/posts', posts);
 
 // Server
 const port = process.env.PORT || 4500;
--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -1,17 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.all('/*', (req, res, next)=>{
     req.app.locals.layout = 'admin';
     next();
 });
 
-
-router.get('/',(req, res)=>{
-	res.render("admin/index");
+router.get('/',(req,res)=>{
+	res.send("It works");
 });
-
-
 
 module.exports = router;
