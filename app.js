3df47b8d817bea0f36ba9ed476b11f48ed034ae5
Masud99Rana <Masudrana917775@gmail.com>
2020-01-25 14:04:04 +0600

    master template added & separated

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-app.listen(4500,()=>{
-	console.log(`listening on port 4500`);
-})
+
+
+// Using Static
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set up View Engine
+app.engine('handlebars',exphbs({defaultLayout: 'home'}) )
+app.set('view engine', 'handlebars'); 
+//handlebars auto search home.hanlebars in layouts folder
+
+
+app.get('/',(req, res)=>{
+	res.render("home/index");
+});
+
+app.get('/about',(req, res)=>{
+	// res.send("home/about");
+	res.render("home/about");
+});
+
+const port = process.env.PORT || 4500;
+app.listen(port, ()=>{
+	console.log(`listening on port ${port}`);
+});
