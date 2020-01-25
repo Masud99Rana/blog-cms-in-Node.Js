49e564ff1dad7d0f21d16b4c45cdf80ff3dbea44
Masud99Rana <Masudrana917775@gmail.com>
2020-01-25 21:50:16 +0600

    code refactor & master layouts added

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 
 
-
 // Using Static
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -13,13 +12,15 @@ app.engine('handlebars',exphbs({defaultLayout: 'home'}) )
 app.set('view engine', 'handlebars'); 
 //handlebars auto search home.hanlebars in layouts folder
 
-
 // Load Routes
-const home = require('./routes/home/index.js');
+const home = require('./routes/home/index');
+const admin = require('./routes/admin/index');
 
 // Use Routes
 app.use('/', home);
+app.use('/admin', admin);
 
+// Server
 const port = process.env.PORT || 4500;
 app.listen(port, ()=>{
 	console.log(`listening on port ${port}`);
