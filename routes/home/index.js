f9d9bb798f47744ff5cd60d473477e86543ed0a6
Masud99Rana <Masudrana917775@gmail.com>
2020-01-26 21:56:51 +0600

    Showed all posts & single posts in frontend

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,13 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const Post  = require('../../models/Post');
+const { uploadDir } = require('../../helpers/upload-helper');
 
 router.all('/*', (req, res, next)=>{
     req.app.locals.layout = 'home';
     next();
 });
 
+// show front page with all posts
 router.get('/',(req, res)=>{
-	res.render("home/index");
+	
+	Post.find({}).then(posts=>{
+		// console.log(posts);
+		const context = {
+	        postsDocuments: posts.map(document => {
+	          return {
+	            id: document._id,
+	            title: document.title,
+	            body: document.body,
+	            status: document.status,
+	            allowComments: document.allowComments,
+	            date: document.date,
+	            file: document.file || "flower.jpg"
+	          }
+	        })
+		};
+		// console.log(context.postsDocuments);
+		      
+	    res.render("home/index", {posts: context.postsDocuments});
+	});
+});
+
+//Show edit post form
+router.get('/post/:id', (req, res)=>{
+	// res.send(req.params.id);
+
+    Post.findOne({_id: req.params.id})
+        .then(post=>{
+        	// console.log(post);
+    		
+    		const postsDocument = {
+	            id: post._id,
+	            title: post.title,
+	            body: post.body,
+	            status: post.status,
+	            allowComments: post.allowComments,
+	            date: post.date,
+	            file: post.file || "flower.jpg"
+			};
+
+         	res.render('home/post', {post: postsDocument});
+    });
 });
 
 router.get('/about',(req, res)=>{
