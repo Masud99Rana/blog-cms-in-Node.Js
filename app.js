5e0c12281ac3b88861eed6e2cdf067cdc7214ed8
Masud99Rana <Masudrana917775@gmail.com>
2020-01-27 00:42:54 +0600

    User registraton feature implemented

--- app.js
+++ app.js
@@ -53,6 +53,10 @@ app.use(flash());
 app.use((req, res, next)=>{
 
     res.locals.success_message = req.flash('success_message');
+    res.locals.error_message = req.flash('error_message');
+    res.locals.form_errors = req.flash('form_errors');
+    res.locals.error = req.flash('error');
+    
     next();
 });
 
--- routes/home/index.js
+++ routes/home/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Post  = require('../../models/Post');
 const Category = require('../../models/Category');
+const User = require('../../models/User');
+const bcrypt = require('bcryptjs');
+
 
 router.all('/*', (req, res, next)=>{
     req.app.locals.layout = 'home';
@@ -81,19 +84,85 @@ router.get('/post/:id', (req, res)=>{
     });
 });
 
-router.get('/about',(req, res)=>{
-	// res.send("home/about");
-	res.render("home/about");
-});
-
 router.get('/login',(req, res)=>{
 	// res.send("home/about");
 	res.render("home/login");
 });
 
+router.post('/login',(req, res)=>{
+	res.send("It works");
+});
+
+
 router.get('/register',(req, res)=>{
 	// res.send("home/about");
 	res.render("home/register");
 });
+router.post('/register',(req, res)=>{
+	// res.send("It works");
+	let errors = [];
+
+    if(!req.body.firstName) {
+        errors.push({message: 'Please enter your first name'});
+    }
+
+    if(!req.body.lastName) {
+        errors.push({message: 'Please add a last name'});
+    }
+
+    if(!req.body.email) {
+        errors.push({message: 'Please add an email'});
+    }
+
+    if(!req.body.password) {
+        errors.push({message: 'Please enter a password'});
+    }
+
+    if(!req.body.passwordConfirm) {
+        errors.push({message: 'This field cannot be blank'});
+    }
+
+    if(req.body.password !== req.body.passwordConfirm) {
+        errors.push({message: "Password fields don't match"});
+    }
+
+    if(errors.length > 0){
+        res.render('home/register', {
+            errors: errors,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+        })
+
+    } else {
+
+        User.findOne({email: req.body.email}).then(user=>{
+
+            if(!user){
+                const newUser = new User({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+
+                bcrypt.genSalt(10, (err, salt)=>{
+                    bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                        newUser.password = hash;
+
+                        newUser.save().then(savedUser=>{
+                            req.flash('success_message', 'You are now registered, please login')
+                            res.redirect('/login');
+                        });
+                    })
+                });
+            } else {
+                req.flash('error_message', 'That email exist please login');
+                res.redirect('/login');
+            }
+        });
+    }
+});
+
 
 module.exports = router;
