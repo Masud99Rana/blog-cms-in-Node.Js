beef254764a4e64fb4d5bd5de8ed71ab99e4757a
Masud99Rana <Masudrana917775@gmail.com>
2020-01-28 01:14:36 +0600

    Admin Chart added

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const faker = require('faker');
 const Post = require('../../models/Post');
+const Category = require('../../models/Category');
+const Comment = require('../../models/Comment');
 const { userAuthenticated } = require('../../helpers/authentication');
 
 router.all('/*', (req, res, next)=>{
@@ -10,10 +12,30 @@ router.all('/*', (req, res, next)=>{
 });
 
 
-router.get('/',(req, res)=>{
+router.get('/', (req, res)=>{
 
-	res.render("admin/index");
+
+    const promises = [
+        Post.count().exec(),
+        Category.count().exec(),
+        Comment.count().exec()
+    ];
+
+    Promise.all(promises).then(([postCount, categoryCount, commentCount])=>{
+        res.render('admin/index',{
+                postCount: postCount,
+                categoryCount: categoryCount, 
+                commentCount: commentCount
+            });
+    });
+
+    
+    // Post.count({}).then(postCount=>{
+    //     res.render('admin/index', {postCount: postCount});  
+    // });
+    
 });
+
 
 router.post('/generate-fake-posts', (req, res)=>{
     for(let i = 0; i < req.body.amount; i++){
