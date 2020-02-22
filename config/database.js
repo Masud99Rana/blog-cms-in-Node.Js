670017849a8bdb10ef01de246d5a8ae70c1038cf
Masud99Rana <Masudrana917775@gmail.com>
2020-02-22 09:00:02 +0600

    mongo atlas added

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
-    mongoDbUrl:'mongodb://localhost:27017/mr-cms'
+    // mongoDbUrl:'mongodb://localhost:27017/mr-cms'
+    mongoDbUrl:'mongodb+srv://cms-user:<oIMXYzcluJHunrRL>@cluster0-hdjzg.mongodb.net/test?retryWrites=true&w=majority'
 
 };
